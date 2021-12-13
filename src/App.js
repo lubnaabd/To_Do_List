c335c914d3ea3bbeb6e4a469f8b721e563ee9513
lubnaabd <lubna_mosa@hotmail.com>
2021-12-13 19:50:50 +0200

    add multi langauge using React i18n library

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import { useSelector, useDispatch } from "react-redux";
 import { getItems,deleteItem } from "./Redux/List/action";
 import React, { useEffect } from "react";
 import ToDoList from "./Components/ToDoList";
+import './Utils/i18n'
+import { useTranslation } from 'react-i18next';
 
 function App() {
   const { isLoading, list, error } = useSelector((state) => state.list);
+  const { t, i18n } = useTranslation();
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getItems());
@@ -24,7 +27,7 @@ function App() {
         <div className="list">
           {list.slice(0,10 ).map((element) => (
             <div className="list_item"key={element.id}>{element.title}
-                  <button onClick={()=>handleDeleteItem(element.id)}  className="delete_item">Delete</button>
+                  <button onClick={()=>handleDeleteItem(element.id)}  className="delete_item">{t('delete')}</button>
 
             </div>
             
--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -2,7 +2,11 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 import { addItem } from "../Redux/List/action";
+import { useTranslation } from "react-i18next";
+import changeLanguage from "../Utils/ChangeLanguage"
 const ToDoList = () => {
+  const { t, i18n } = useTranslation();
+
   const [value, setValue] = useState("");
   const dispatch = useDispatch();
   const handleInputChange = (e) => setValue(e.target.value);
@@ -16,12 +20,25 @@ const ToDoList = () => {
     );
     setValue("");
   };
-
-  
+  const handleChangeLanguage=()=>{
+    changeLanguage(i18n.language==='ar'?"en":'ar' ,i18n)
+  }
   return (
     <div>
-      <input className="input_to_list" type={"text"} onChange={handleInputChange} value={value}></input>
-      <button onClick={handleAddItem} className="add_item">Add Item</button>
+      <h1>{t("toDolist")}</h1>
+      <button onClick={handleChangeLanguage} className="add_item">
+        {t("language")}
+      </button>
+
+      <input
+        className="input_to_list"
+        type={"text"}
+        onChange={handleInputChange}
+        value={value}
+      ></input>
+      <button onClick={handleAddItem} className="add_item">
+        {t("add")}
+      </button>
     </div>
   );
 };
